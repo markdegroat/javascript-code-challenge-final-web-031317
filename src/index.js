c5d92c55690bc2f1bfe52608dc04a456d4b654a6
markdegroat <mhdegroat@gmail.com>
2017-05-12 09:49:58 -0400

    Core MVP is now working, commiting before I separate concerns and refactor current code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   // your code here!
+  $("#photo-form").on("submit", function(e){
+    e.preventDefault()
+    const image_url = $("#image-url").val()
+    const caption_text = $("#caption-text").val()
+    $("#photo-list").append(`<img src="${image_url}"></img>`)
+    $("#photo-list").append(`${caption_text}`)
+
+  })
 })
