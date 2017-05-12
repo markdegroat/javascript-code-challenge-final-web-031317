8a570cf9865b6cf8e59ec93c38ae1ae0a1552a9f
markdegroat <mhdegroat@gmail.com>
2017-05-12 10:34:09 -0400

    Changed fixed image size to 400x600px

--- src/views/pictureView.js
+++ src/views/pictureView.js
@@ -4,7 +4,7 @@ class PictureView{
     //maybe do something here eventually
   }
   render(picture){
-    $("#photo-list").append(`<img src="${picture.image_url}" height="200px" width="300px"></img>`)
+    $("#photo-list").append(`<img src="${picture.image_url}" height="400px" width="600px"></img>`)
     $("#photo-list").append(`<div>${picture.caption_text}<div>`)
   }
 }
